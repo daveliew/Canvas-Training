28abf5a7ee17268ddd1a77dd1bef36089d78b04f
daveliew <daveliew@gmail.com>
2021-06-10 22:44:45 +0800

    created particle init(), and particle helper function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const ctx = canvas.getContext("2d"); //* add context via ctx
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  const particleArray = [];
 
   window.addEventListener("resize", () => {
     canvas.width = window.innerWidth;
@@ -15,26 +16,62 @@ document.addEventListener("DOMContentLoaded", function (event) {
   };
 
   //* passes mouse click coordinates to global variable
-  canvas.addEventListener("click", (event) => {
-    mouse.x = event.x;
-    mouse.y = event.y;
+  // canvas.addEventListener("click", (event) => {
+  //   mouse.x = event.x;
+  //   mouse.y = event.y;
+  //   drawCircle();
+  // });
 
-    drawCircle();
-  });
+  // canvas.addEventListener("mousemove", (event) => {
+  //   mouse.x = event.x;
+  //   mouse.y = event.y;
+  //   drawCircle();
+  // });
 
-  canvas.addEventListener("mousemove", (event) => {
-    mouse.x = event.x;
-    mouse.y = event.y;
-    drawCircle();
-  });
+  class Particle {
+    constructor() {
+      // this.x = mouse.x;
+      // this.y = mouse.y;
+      this.x = Math.random() * canvas.width;
+      this.y = Math.random() * canvas.height;
 
-  const drawCircle = () => {
-    ctx.fillStyle = "blue";
-    ctx.strokeStyle = "red";
-    ctx.lineWidth = 2;
-    ctx.beginPath(); //* like a paint path
-    ctx.arc(mouse.x, mouse.y, 10, 0, Math.PI * 2);
-    ctx.fill();
+      this.size = Math.random() * 5 + 1;
+      this.speedX = Math.random() * 3 - 1.5; //* create -ve and +ve vector
+      this.speedY = Math.random() * 3 - 1.5;
+    }
+    update() {
+      this.x += this.speedX; //* 2D vector creation
+      this.y += this.speedY;
+    }
+    draw() {
+      ctx.fillStyle = "blue";
+      ctx.beginPath(); //* like a paint path
+      ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
+      ctx.fill();
+    }
+  }
+
+  const init = () => {
+    for (let i = 0; i < 100; i++) {
+      particleArray.push(new Particle());
+    }
   };
-  drawCircle();
+
+  init();
+
+  const handleParticles = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      particleArray[i].update();
+      particleArray[i].draw();
+    }
+  };
+
+  //* Animate generations
+
+  const animate = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handleParticles(particleArray);
+    requestAnimationFrame(animate);
+  };
+  animate();
 });
