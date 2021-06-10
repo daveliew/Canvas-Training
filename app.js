a318b79a39ff840546281b2a6cd7a5583566b45a
daveliew <daveliew@gmail.com>
2021-06-10 21:07:36 +0800

    add window event listener to handle browser resizing

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d"); //* add context via ctx
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-  console.log(ctx);
+  window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    //*resizing will destory any drawings
+    ctx.fillStyle = "white";
+    ctx.fillRect(10, 10, 150, 150);
+  });
 
   ctx.fillStyle = "white";
   ctx.fillRect(10, 10, 150, 150);
